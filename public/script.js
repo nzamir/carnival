986ee1149720beb91551117fc401b5183a4ef4e4
nzamir <nzamir91@gmail.com>
2025-09-29 23:21:55 +0800

    fix

--- public/script.js
+++ public/script.js
@@ -71,8 +71,6 @@ document.getElementById('task-form').addEventListener('submit', async function (
 
     const result = await res.json();
     alert(result.message);
-    document.getElementById('task-form').reset();
-    document.getElementById('task-options').innerHTML = '';
   } catch (err) {
     console.error('Error submitting form:', err);
     alert('Submission failed.');
